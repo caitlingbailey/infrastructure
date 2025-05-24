679bb2c0dae76562af9fbe0d45cf237c4649b97f
Caitlin Bailey <caitlin.bailey@starlingbank.com>
2025-05-24 22:38:37 +0100

    move files to terraform folder

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type = string
+  default = "eu-west-1"
+}
+
+variable "s3_name" {
+  type = string
+  default = "caitlingbailey-website"
+}
+
+variable "root_domain_name" {
+  type = string
+  default = "caitlingbailey.com"
+}
