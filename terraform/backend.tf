a05aef4990788d4e2a67cabf3949a713aa9ef9cd
Caitlin Bailey <caitlin.bailey@starlingbank.com>
2025-05-26 15:23:03 +0100

    Format

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "caitlingbailey.com-terraform"
-    region         = "eu-west-1"
-    key            = "Non-Modularized/S3-Static-Website/terraform.tfstate"
-    encrypt        = true
+    bucket  = "caitlingbailey.com-terraform"
+    region  = "eu-west-1"
+    key     = "Non-Modularized/S3-Static-Website/terraform.tfstate"
+    encrypt = true
   }
   required_version = ">=0.13.0"
   required_providers {
