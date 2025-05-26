cc8af5cc00b96b0c967e06d8123f2c97d46372d5
Caitlin Bailey <caitlin.bailey@starlingbank.com>
2025-05-26 16:02:39 +0100

    Add S3 bucket

--- terraform/s3-bucket.tf
+++ terraform/s3-bucket.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "s3-bucket" {
   }
 }
 
-resource "aws_s3_bucket" "s3-bucket" {
+resource "aws_s3_bucket" "s3-bucket-test" {
   bucket        = "caitlingbailey-yesy"
   force_destroy = true
   lifecycle {
