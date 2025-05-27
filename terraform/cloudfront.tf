7fd3d9c02f6aeb4c9e4302ad2c8852e415f80b8b
Caitlin Bailey <caitlingbailey@aol.com>
2025-05-27 16:50:44 +0100

    Cloudfront tweaks attempt 4 (#23)
    * Tweaks to Cloudfront distribution
    
    * Dont delete resource
    
    * Remove cache policy

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,3 +1,4 @@
+# CloudFront origin access control for S3 origin type with always signing using sigv4 protocol
 resource "aws_cloudfront_origin_access_control" "website_origin_access_control" {
   name                              = "${var.domain_name} Access Control Policy"
   description                       = "Cloudfront access control policy for the ${var.domain_name} distribution."
@@ -11,54 +12,60 @@ resource "aws_cloudfront_distribution" "cdn_static_website" {
   enabled             = true
   is_ipv6_enabled     = true
   default_root_object = "index.html"
+  aliases             = ["${var.domain_name}", "www.${var.domain_name}"]
+  price_class         = "PriceClass_100"
+  wait_for_deployment = true
 
   origin {
     domain_name              = aws_s3_bucket.s3-bucket.bucket_regional_domain_name
-    origin_id                = "my-s3-origin"
-    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = var.domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.website_origin_access_control.id
   }
 
   default_cache_behavior {
-    min_ttl                = 0
-    default_ttl            = 0
-    max_ttl                = 0
     viewer_protocol_policy = "redirect-to-https"
-
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "my-s3-origin"
-
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
+    compress               = true
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "my-s3-origin"
+    smooth_streaming       = false
   }
 
   restrictions {
     geo_restriction {
-      locations        = []
       restriction_type = "none"
     }
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.certificate.arn
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
+    acm_certificate_arn            = aws_acm_certificate.certificate.arn
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
+    cloudfront_default_certificate = false
   }
 
-  aliases = ["${var.domain_name}", "www.${var.domain_name}"]
+  custom_error_response {
+    error_code         = "404"
+    response_code      = "200"
+    response_page_path = "/index.html"
+  }
+  custom_error_response {
+    error_code         = "403"
+    response_code      = "200"
+    response_page_path = "/index.html"
+  }
 }
 
-# CloudFront origin access control for S3 origin type with always signing using sigv4 protocol
 resource "aws_cloudfront_origin_access_control" "default" {
   name                              = "cloudfront OAC"
   description                       = "description OAC"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
+}
+
+data "aws_cloudfront_cache_policy" "cache_policy" {
+  name = "Managed-CachingOptimized"
 }
 
 # Output the CloudFront distribution URL using the domain name of the cdn_static_website resource.
